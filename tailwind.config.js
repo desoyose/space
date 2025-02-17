8544596ac1c752ed018ada111ef16fe3d7ffcd23
[Jorge Navaridas] <jorgenavaridas08@gmail.com>
2025-02-17 12:19:07 +0100

    Eliminada la carpeta 'space' del índice de Git

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,21 @@
-// tailwind.config.js
 module.exports = {
   content: [
-    "./index.html",
-    "./pages/**/*.html",  // Asegúrate de incluir todos los archivos HTML
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./index.html",            // Incluye el archivo index.html
+    "./pages/**/*.html",       // Incluye todos los archivos .html dentro de la carpeta pages (fuera de src)
+    "./src/**/*.{js,ts,jsx,tsx}",  // Incluye todos los archivos JS/TS en src
   ],
   theme: {
     extend: {
       colors: {
         "Blue-sky": "#3E54A3",
-
-        "Blue_Dark" : "#1E1E1E",
+        "Blue_Dark": "#1E1E1E",
         "Just_black": "#0B0D17",
         "Blue_purple": "#D0D6F9",
-        
       },
-    
       fontFamily: {
-        barlow: ['Barlow', 'sans-serif'],  
-        barlowCondensed: ['Barlow Condensed', 'sans-serif'],  
-        bellafair:['Bellefair', 'sans'],
+        barlow: ['Barlow', 'sans-serif'],
+        barlowCondensed: ['Barlow Condensed', 'sans-serif'],
+        bellafair: ['Bellefair', 'sans'],
       },
     },
   },
